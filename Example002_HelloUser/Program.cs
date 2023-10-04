81e2863416486fc6924d0dd0b786691c52713ac1
RomanSH <roman_2679@mail.ru>
2023-10-04 20:07:14 +0300

    add Example 002Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// Ввод имени и приветствие
+Console.Write.WriteLine("Введите ваше имя "); 
+string username = Console.ReadLine();
+Console.Write.WriteLine("Привет, ");
+Console.Write.WriteLine(username);
