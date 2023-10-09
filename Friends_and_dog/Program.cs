1ac2a3af93068b504069b369c895369212af44b4
RomanSH <roman_2679@mail.ru>
2023-10-09 15:43:27 +0300

    Задачи 2 лекция

---
+++ Friends_and_dog/Program.cs
@@ -0,0 +1,25 @@
+﻿// Друзья и собака
+int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+while (distance > 10)
+{
+    int time = 0;
+    if (friend == 1)
+    {
+        
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    else    
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }  
+distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+count = count + 1;
+}
+Console.WriteLine(count);
---
+++ Seminar01/Program.cs
@@ -0,0 +1,86 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Введено не трехзначное число");
+    return;
+} 
+Console.WriteLine(number%10);
+
+
+
+
+
+
+
+
+
+
+// // Вывод квадрата числа
+// Console.Write("Введите число: ");
+// // int number1 = int.Parse(Console.ReadLine());
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqr = number * number;
+// Console.WriteLine(sqr);
+
+// Console.WriteLine("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 == number2 * number2)
+// {
+//     Console.WriteLine("Первое число является квадратом второго ");
+// }
+// else
+// {
+//     Console.WriteLine("Первое число НЕ является квадратом второго ");
+// }
+
+// Console.WriteLine("Введите номер дня недели: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if (day == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// } 
+// else if(day == 2)
+// {
+//      Console.WriteLine("Вторник");
+// }
+// else if(day == 3)
+// {
+//      Console.WriteLine("Среда");
+// }     
+// else if(day == 4)
+// {
+//      Console.WriteLine("Четверг");
+// }
+// else if(day == 5)
+// {
+//      Console.WriteLine("Пятница");
+// }
+// else if(day == 6)
+// {
+//      Console.WriteLine("Суббота");
+// }
+// else if(day == 7)
+// {
+//      Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Нет такого дня недели");
+// }
+
+// Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int index = -number;
+// while (index <= number)
+// {
+//     Console.Write(index+" ");
+//     index++;
+// }
+
