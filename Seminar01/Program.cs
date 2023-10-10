8f71848dfb3c0d408b0a215df78ea74bf7f9e321
RomanSH <roman_2679@mail.ru>
2023-10-10 11:14:44 +0300

    2_1

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -1,11 +1,82 @@
-﻿Console.WriteLine("Введите трехзначное число: ");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number < 100 || number > 999)
+
+if (number % 7 == 0 && number % 23 == 0)
 {
-    Console.WriteLine("Введено не трехзначное число");
-    return;
-} 
-Console.WriteLine(number%10);
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратн
+
+
+// Console.WriteLine("Введите первое число");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+// if (firstNumber % secondNumber == 0)
+// {
+//     Console.WriteLine("кратное");
+// }
+// else
+// {
+//     Console.WriteLine($"НЕт, {firstNumber % secondNumber}" );
+// }
+
+
+
+
+
+//  Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+// Random rand = new Random();
+// int randomNumber = rand.Next(100, 1000);
+// int leftDigit = randomNumber / 100;
+// int rightDigit = randomNumber % 10;
+// int result = leftDigit*10+rightDigit;
+// Console.WriteLine(randomNumber + " -> " + result);
+
+// int randomNumber = new Random().Next(10, 100);
+// Console.WriteLine(randomNumber);
+// // int left = randomNumber / 10;
+// // int right = randomNumber % 10;
+// if (randomNumber / 10 > randomNumber % 10)
+// {
+//     Console.WriteLine(randomNumber / 10);
+// }
+// else
+// {
+//     Console.WriteLine(randomNumber % 10);
+// }
+
+
+// Console.WriteLine("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 100 || number > 999)
+// {
+//     Console.WriteLine("Введено не трехзначное число");
+//     return;
+// } 
+// Console.WriteLine(number%10);
 
 
 
